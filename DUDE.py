a274da223d7963a418ed0254a1265190e5e1c9cc
Kevin Ly <kevinly2222@yahoo.com>
2021-09-06 01:03:48 -0700

    fix

--- DUDE.py
+++ DUDE.py
@@ -44,16 +44,13 @@ def discordInit(DISCORD_TOKEN):
 
     @bot.command(name='play', help=f'{symbol} play <song_link>')
     async def play(ctx,url):
-        try:
-            voice_client = ctx.message.guild.voice_client
-            server = ctx.message.guild
-            voice_channel = server.voice_client
-            async with ctx.typing():
-                filename = await YTDLSource.from_url(url, loop=bot.loop)
-                voice_channel.play(discord.FFmpegPCMAudio(source=filename))
-            await ctx.send(f'**Now playing:** {filename}')
-        except:
-            await ctx.send(f'Put me in a channel')
+        voice_client = ctx.message.guild.voice_client
+        server = ctx.message.guild
+        voice_channel = server.voice_client
+        async with ctx.typing():
+            filename = await YTDLSource.from_url(url, loop=bot.loop)
+            voice_channel.play(discord.FFmpegPCMAudio(source=filename))
+        await ctx.send(f'**Now playing:** {filename}')
 
     @bot.command(name='join', help='Bot joins voice channel')
     async def join(ctx):
